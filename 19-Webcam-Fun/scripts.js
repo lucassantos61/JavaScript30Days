43a8156731cb962b511252d64b8cfcafb19a90d2
Lucas Ferreira <lucasferreira241@gmail.com>
2019-04-10 23:45:31 -0300

    #day-ninateen -  finish webcam fun

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -8,7 +8,8 @@ const snap = document.querySelector('.snap');
 function getVideo() {
     navigator.mediaDevices.getUserMedia({video : true , audio : false})
     .then(localMediaStream => {
-        video.src = window.URL.createObjectURL(localMediaStream);
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
         video.play();
     })
     .catch(err => {
@@ -16,4 +17,76 @@ function getVideo() {
     });
 }
 
+function paintCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = greenScrean(pixels);
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+    console.log(width, height);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man">`
+    strip.insertBefore(link, strip.firsChild)
+ 
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100;
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+}
+
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 500] = pixels.data[i + 1];
+        pixels.data[i - 550] = pixels.data[i + 2];
+    }
+    return pixels;    
+}
+
+function greenScrean(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+    console.log(levels);
+    for (i = 0; i < pixels.data.length; i = i + 1) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    
+        if ( red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red >= levels.rmax
+            && green >= levels.gmax
+            && blue >= levels.bmax) {
+                pixels.data[i + 3] = 0;
+            }
+    }
+    return pixels;
+}
 getVideo();
+
+video.addEventListener('canplay',paintCanvas)
